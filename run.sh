65c947559d90fdcf679ae30b63f78f99c36a73b2
LT1025 <liutong20@mails.ucas.ac.cn>
2021-12-14 10:58:18 +0800

    Judge bad file path

--- run.sh
+++ run.sh
@@ -2,27 +2,29 @@ make
 
 # Test Data 1
 # Template:150*150*200     Raw Image:512*512
-# ./main  --input Data/Data_1/sample_bin2_filter_input.lst \
-#         --angpix 2.08 \
-#         --template Data/Data_1/sample_step5_p1.hdf \
-#         --eulerfile Data/Data_1/global_euler_step5_p1.lst \
-#         --phistep 2 \
-#         --weight Data/Data_1/wiener_reo.txt \
-#         --kk 3 \
-#         --first 0 \
-#         --last 1 \
-#         --energy 300 \
-#         --cs 2.7 \
-#         --Highres 8 \
-#         --Lowres 100 \
-#         --diameter 180 \
-#         --threshold 7 \
-#         --output Output/test_out.lst \
-#         --padding_size 256 \
-#         --device 7  
+if [ $1 == '0' ]; then
+./main  --input Data/Data_1/sample_bin2_filter_input.lst \
+        --angpix 2.08 \
+        --template Data/Data_1/sample_step5_p1.hdf \
+        --eulerfile Data/Data_1/global_euler_step5_p1.lst \
+        --phistep 2 \
+        --weight Data/Data_1/wiener_reo.txt \
+        --kk 3 \
+        --first 0 \
+        --last 1 \
+        --energy 300 \
+        --cs 2.7 \
+        --Highres 8 \
+        --Lowres 100 \
+        --diameter 180 \
+        --threshold 7 \
+        --output Output/test_out.lst \
+        --padding_size 256 \
+        --device 0  
 
 # Test Data 2
 # Template:182*182*2295    Raw Image:5760*4092
+elif [ $1 == '1' ]; then
 ./main  --input Data/Data_2/test.lst \
         --angpix 1.688 \
         --template Data/Data_2/emd_9976_apix3p336_proj.hdf \
@@ -40,25 +42,26 @@ make
         --threshold 7 \
         --output  Output/test_out.lst \
         --padding_size 320 \
-        --device 5
+        --device 0
 
 # Test  performance 
-# nvprof ./main --input Data/Data_2/test.lst \
-#         --angpix 1.688 \
-#         --template Data/Data_2/emd_9976_apix3p336_proj.hdf \
-#         --eulerfile  Data/Data_2/proj_step3_c2.lst \
-#         --phistep 2 \
-#         --weight Data/Data_2/snr_ncov_search.txt \
-#         --kk 3 \
-#         --first 0 \
-#         --last 1 \
-#         --energy 300 \
-#         --cs 2.7 \
-#         --Highres 8 \
-#         --Lowres 100 \
-#         --diameter 180 \
-#         --threshold 7 \
-#         --output  Output/test_out.lst \
-#         --padding_size 320 \
-#         --device 7
-
+elif [ $1 == '2' ]; then
+nvprof ./main --input Data/Data_2/test.lst \
+        --angpix 1.688 \
+        --template Data/Data_2/emd_9976_apix3p336_proj.hdf \
+        --eulerfile  Data/Data_2/proj_step3_c2.lst \
+        --phistep 2 \
+        --weight Data/Data_2/snr_ncov_search.txt \
+        --kk 3 \
+        --first 0 \
+        --last 1 \
+        --energy 300 \
+        --cs 2.7 \
+        --Highres 8 \
+        --Lowres 100 \
+        --diameter 180 \
+        --threshold 7 \
+        --output  Output/test_out.lst \
+        --padding_size 320 \
+        --device 0
+fi
