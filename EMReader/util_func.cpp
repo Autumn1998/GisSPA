198f5987b7a91e55e8f873aa795a77f900d5b951
LT1025 <liutong20@mails.ucas.ac.cn>
2022-11-17 15:39:05 +0800

    norm after rotating

--- EMReader/util_func.cpp
+++ EMReader/util_func.cpp
@@ -94,11 +94,10 @@ void printHelpMsg()
 	printf("cs               = spherical aberration in um.\n");
 	printf("Highres          = high resolution cut \n");
 	printf("Lowres           = low resolution cut\n");
-	printf("diameter         = target diameter in pixel\n");
+	printf("diameter         = target diameter in pixels\n");
 	printf("Optional Parameters:\n");
-        printf("norm_type        = CCG norm type, 0/ 1/ 2\n");
-        printf("replace_hot        = 0, skip; 1, replace\n");
-        printf("maximum_n_sigma        = values larger than mean by n sigmas will be replaced\n");
+        printf("norm_type        = CCG norm type, 0/ 1\n");
+        printf("invert           = 0 / 1, choose whether to invert contrast, 0 for skipping and 1 for invert\n");
 	printf("threshold        = cc threshold value, only output score beyond this value\n");
 	printf("output           = output lstfile filaname\n");
 	printf("first            = the first image id to process.\n");
